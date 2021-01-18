806994d0e35b85a5a74458a985b0a18aec1358f4
Bethany Fulcher <fulcherbethany@gmail.com>
2021-01-18 14:53:07 -0800

    added full functionality with unique ID for each note

--- server.js
+++ server.js
@@ -16,37 +16,40 @@ app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-app.get("*", (req,res) => {
+app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.get("/api/notes", (req,res) => {
 
-    const str = fs.readFileSync(dbPath, function(err, data) {
-        if (err) throw err;
-        console.log(data)
+    fs.readFile(path.join(__dirname,"db","db.json"), "utf-8", (err,data)=>{
+        if(err) throw err
+        res.json(JSON.parse(data))
     })
-    return res.json(JSON.parse(str));
+
+    // const str = fs.readFileSync(dbPath, function(err, data) {
+    //     if (err) throw err;
+    //     console.log(data)
+    // })
+    // return res.json(JSON.parse(str));
 
 })
 
 app.post("/api/notes", (req,res) => {
-    idCounter++;
     const saveTo = {
         title: req.body.title,
-        text: req.body.text,
-        id: idCounter
+        text: req.body.text
     }
-    console.log(idCounter)
-    fs.readFile("db/db.json", "utf-8", (err,data)=> {
+    fs.readFile(path.join(__dirname,"db","db.json"), "utf-8", (err,data)=> {
         if(err) {
             return res.sendStatus(500);
         }
         let readFrom = JSON.parse(data);
+        saveTo.id = readFrom.length + 1
         readFrom.push(saveTo);
         console.log(readFrom);
 
-        fs.writeFile("db/db.json", JSON.stringify(readFrom), (err) => {
+        fs.writeFile(path.join(__dirname,"db","db.json"), JSON.stringify(readFrom), (err) => {
             if(err) throw err;
             console.log("Done")
             // res.json(readFrom)
@@ -59,7 +62,7 @@ app.post("/api/notes", (req,res) => {
 app.delete("/api/notes/:id", function(req,res) {
 
     const array = JSON.parse (
-        fs.readFileSync("/db/db.json", function(err,data) {
+        fs.readFileSync(path.join(__dirname,"db","db.json"), function(err,data) {
             if (err) throw err;
         })
     )
@@ -67,7 +70,7 @@ app.delete("/api/notes/:id", function(req,res) {
     const deleteID = array.findIndex(i => i.id == req.params.id);
     array.splice(deleteID, 1);
 
-    fs.writeFileSync("/db/db.json", JSON.stringify(array), function(err) {
+    fs.writeFileSync(path.join(__dirname,"db","db.json"), JSON.stringify(array), function(err) {
         if (err) throw err;
     })
 
